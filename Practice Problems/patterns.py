0e35c0a3825a6ddc1ba3e17f23de86ba39dbe9d8
Pavan <loyola.pavan.74@gmail.com>
2026-07-24 18:35:57 +0530

    Patterns

---
+++ Practice Problems/patterns.py
@@ -0,0 +1,24 @@
+rows = 4
+
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+    rows = 4
+num = 1
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num = num + 1
+    print()
+
+rows = 6
+letter = 65
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(chr(letter), end=" ")
+        letter = letter + 1
+    print()
