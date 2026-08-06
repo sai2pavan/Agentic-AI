cf764e44443cb911517652086f035c5d3cfbf3d5
Pavan <loyola.pavan.74@gmail.com>
2026-08-06 15:01:09 +0530

    Sends bulk emails

--- Projects/Email_Automation/EmailAutomation.py
+++ Projects/Email_Automation/EmailAutomation.py
@@ -1,33 +1,34 @@
-#in this file we will use email library
-
 import smtplib
-import random
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-frommail = 'loyola.pavan.74@gmail.com'
-tomail = 'bestfriendpavan2003@gmail.com'
-subject = "Your OTP (One Time Password) for Login"
-otp = random.randint(100000,999999)
-message = f"Your OTP for login is {otp}"
+sender = "loyola.pavan.74@gmail.com"
+password = "hhvb zljw pelr asjb"
 
-msg = MIMEMultipart()
-msg['From'] = frommail
-msg['To'] = tomail
-msg['Subject'] = subject
-msg.attach(MIMEText(message))
-text = msg.as_string()
+recipients = [
+    "bestfriendpavan2003@gmail.com",
+    "pavanpusapati07@gmail.com",
+    "ppusapat@student.gitam.edu"
+]
 
-server = smtplib.SMTP('smtp.gmail.com',587)
+subject = "Test Mail"
+message = "This is a Test mail, Please Ignore it"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
-server.login('loyola.pavan.74@gmail.com','hhvb zljw pelr asjb')
-print('login successfull')
-server.sendmail(frommail,tomail,text)
-print("success")
+server.login(sender, password)
 
-check = int(input("Enter OTP:"))
-if check  == otp:
-    print("OTP valid")
-else:
-    print("Incorrect OTP")
+print("Login Successful")
+
+for recipient in recipients:
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = recipient
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(message, "plain"))
+
+    server.sendmail(sender, recipient, msg.as_string())
+
+server.quit()
+print("All emails sent successfully.")
