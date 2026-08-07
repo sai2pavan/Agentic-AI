1c140c2ef3206ea28ea45e727a217934213d3bc5
Pavan <loyola.pavan.74@gmail.com>
2026-08-07 11:38:45 +0530

    Bulk email automation

---
+++ Python Module/day27.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+sender = "loyola.pavan.74@gmail.com"
+password = input("Enter your password?")
+
+recipients = [
+    "bestfriendpavan2003@gmail.com",
+    "pavanpusapati07@gmail.com",
+    "ppusapat@student.gitam.edu"
+]
+
+subject = "Test Mail"
+message = "This is a Test mail, Please Ignore it"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender, password)
+
+print("Login Successful")
+
+for recipient in recipients:
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = recipient
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(message, "plain"))
+
+    server.sendmail(sender, recipient, msg.as_string())
+
+server.quit()
+print("All emails sent successfully.")
